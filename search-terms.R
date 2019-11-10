b762a2c7d1e14d3efb52f37c0af92a54a8527173
Hadley Wickham <h.wickham@gmail.com>
2019-11-10 09:11:53 -0600

    Update search terms script

--- search-terms.R
+++ search-terms.R
@@ -1,47 +1,20 @@
 library(tidyverse)
-source("api-analytics.R")
-
-alphabetise <- function(x) {
-  r_match <- "\\b(in )?r\\b"
-  has_r <- str_detect(x, r_match)
-
-  out <- x %>%
-    str_replace_all(r_match, "") %>%
-    str_squish() %>%
-    str_split(" ") %>%
-    map(str_sort) %>%
-    map_chr(str_flatten, " ")
-
-  out[has_r] <- paste0(out[has_r], " [r]")
-  out
-}
-
-group_terms <- function(df) {
-  df %>%
-    mutate(query_a = alphabetise(query)) %>%
-    group_by(query_a) %>%
-    summarise(
-      query = list(query),
-      clicks = sum(clicks),
-      position = weighted.mean(position, impressions),
-      impressions = sum(impressions),
-    ) %>%
-    select(query_a, query, clicks, impressions, position) %>%
-    mutate(ctr = clicks / impressions) %>%
-    arrange(desc(clicks))
-}
+source("utils/search-console.R")
 
 readxl <- sc_get_queries("readxl.tidyverse.org") %>%
   filter(clicks > 0) %>%
   group_terms()
+readxl
 
-readxl %>% ggplot(aes(ctr, wt = impressions)) + geom_histogram(binwidth = 0.02)
+readxl %>%
+  ggplot(aes(ctr, wt = impressions)) +
+  geom_histogram(binwidth = 0.02)
 
 # Code searches tend to have high ctr?
 readxl %>% filter(str_detect(query_a, "_"))
 
 # See original queries
-grouped %>%
+readxl %>%
   filter(query_a == "package readxl [r]") %>%
   pull(query) %>%
   .[[1]]
--- utils/search-console.R
+++ utils/search-console.R
@@ -1,16 +1,15 @@
+library(searchConsoleR)
+searchConsoleR::scr_auth(email = TRUE)
 
 # suppress broken check
 assignInNamespace("check.Url", function(url, ...) url, "searchConsoleR")
 
 # "today" in search console time is 3 days ago
-today <- function() lubridate::today() - lubridate::days(3)
-last_week <- function() today() - lubridate::weeks(1)
-last_month <- function() today() - lubridate::period(months = 1)
-last_quarter <- function() today() - lubridate::months(3)
-last_year <- function() today() - lubridate::years(1)
 
 sc_get <- function(dimension, filter = NULL, from = last_month(), to = today()) {
   # dimension <- rlang::arg_match(dimension, c("query", "date", "country", "device", "page"))
+  today <- function() lubridate::today() - lubridate::days(3)
+  last_month <- function() today() - lubridate::period(months = 1)
 
   out <- suppressMessages(searchConsoleR::search_analytics(
     "sc-domain:tidyverse.org",
@@ -31,4 +30,34 @@ sc_get_queries <- function(url, from = last_month(), to = today()) {
 sc_get_pages <- function(query, from = last_month(), to = today()) {
   filter <- paste0("query==", query)
   sc_get(dimension = "page", filter = filter, from = from, to = to)
+}
+
+alphabetise <- function(x) {
+  r_match <- "\\b(in )?r\\b"
+  has_r <- str_detect(x, r_match)
+
+  out <- x %>%
+    str_replace_all(r_match, "") %>%
+    str_squish() %>%
+    str_split(" ") %>%
+    map(str_sort) %>%
+    map_chr(str_flatten, " ")
+
+  out[has_r] <- paste0(out[has_r], " [r]")
+  out
+}
+
+group_terms <- function(df) {
+  df %>%
+    mutate(query_a = alphabetise(query)) %>%
+    group_by(query_a) %>%
+    summarise(
+      query = list(query),
+      clicks = sum(clicks),
+      position = weighted.mean(position, impressions),
+      impressions = sum(impressions),
+    ) %>%
+    select(query_a, query, clicks, impressions, position) %>%
+    mutate(ctr = clicks / impressions) %>%
+    arrange(desc(clicks))
 }
