b43d2509a590f2b014de4fa6d6f675707a74ce81
Hadley Wickham <h.wickham@gmail.com>
2019-11-11 16:37:27 -0600

    Polishing overview

--- overview.R
+++ overview.R
@@ -26,10 +26,14 @@ weekly %>%
   ) +
   theme(legend.position = "bottom")
 
+# Dangerous to read too much into this plot as it's taken some time to
+# get all major sites using google analytics.
+# * tidytemplate accientally conditioned out analytics on Sep 3, and
+#   wasn't fully restored until Nov 10.
+
 # By site -----------------------------------------------------------------
 
 by_site <- analytics("hostname")
-
 by_site %>%
   filter(is_tidyverse(hostname)) %>%
   ggplot(aes(fct_reorder(hostname, users), users)) +
@@ -47,23 +51,19 @@ by_site %>%
 
 site_weekly <- analytics_weekly("hostname")
 
-
-# Top
+# Weekly sessions for top sites
 site_weekly %>%
   filter(is_tidyverse(hostname)) %>%
-  group_by(week, hostname = fct_lump(hostname, n = 7, w = sessions)) %>%
-  summarise(n = sum(sessions)) %>%
-  ungroup() %>%
-  mutate(hostname = fct_reorder2(hostname, week, n)) %>%
-  ggplot(aes(week, n, colour = hostname)) +
+  lump_var(hostname) %>%
+  ggplot(aes(week, sessions, colour = hostname)) +
   geom_line(size = 1) +
   labs(title = "Weekly sessions", x = NULL, y = "Sessions", colour = NULL) +
   scale_y_continuous(labels = scales::comma) +
-  scale_colour_brewer(palette = "Set2") +
-  theme(legend.position = "bottom")
+  scale_colour_brewer(palette = "Set1") +
+  theme(legend.position = "right", legend.justification = "top")
 
 site_sessions <- function(hostname) {
-  by_site %>%
+  site_weekly %>%
     filter(.data$hostname %in% c(.env$hostname)) %>%
     ggplot(aes(week, sessions, colour = fct_reorder2(hostname, week, sessions))) +
     geom_line(size = 1) +
@@ -75,13 +75,13 @@ site_sessions <- function(hostname) {
       y = "Sessions",
       colour = NULL
     ) +
+    scale_colour_brewer(palette = "Set1") +
     theme(legend.position = "bottom")
 }
 site_sessions(c("dplyr.tidyverse.org", "r4ds.had.co.nz", "ggplot2.tidyverse.org"))
 site_sessions(c("readxl.tidyverse.org", "readr.tidyverse.org", "haven.tidyverse.org", "vroom.r-lib.org"))
-site_sessions(c("tidyr.tidyverse.org", "stringr.tidyverse.org"))
+site_sessions(c("tidyr.tidyverse.org", "stringr.tidyverse.org", "purrr.tidyverse.org"))
 site_sessions(c("lubridate.tidyverse.org", "forcats.tidyverse.org", "tibble.tidyverse.org"))
 site_sessions(c("httr.r-lib.org", "rvest.tidyverse.org", "xml2.r-lib.org"))
-site_sessions(c("magrittr.tidyverse.org", "glue.tidyverse.org", "purrr.tidyverse.org"))
+site_sessions(c("magrittr.tidyverse.org", "glue.tidyverse.org", "reprex.tidyverse.org"))
 site_sessions(c("usethis.r-lib.org", "roxygen2.r-lib.org", "testthat.r-lib.org", "devtools.r-lib.org", "pkgdown.r-lib.org"))
-site_sessions("reprex.tidyverse.org")
