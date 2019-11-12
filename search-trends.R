eca8a8f919f4a8de8a05eddb9db55f20c2839dae
Hadley Wickham <h.wickham@gmail.com>
2019-11-12 10:12:36 -0600

    Tweak search terms

--- search-trends.R
+++ search-trends.R
@@ -2,20 +2,17 @@ library(tidyverse)
 # remotes::install_github("josiahparry/gtrendsR", "interest_refactor")
 library(gtrendsR)
 
-ob <- gtrends(c("ggplot2", "dplyr"))
+ob <- gtrends(c("ggplot2", "dplyr", "tidyverse"))
 df <- as_tibble(ob$interest_over_time)
 df %>%
   filter(hits > 0) %>%
   ggplot(aes(date, hits, colour = keyword, group = keyword)) +
   geom_line(colour = "grey80") +
   geom_smooth(se = FALSE) +
-  scale_y_log10()
-
-ob <- gtrends(c("dplyr", "data.table"), time = "all")
-df <- as_tibble(ob$interest_over_time)
-df$hits <- as.numeric(df$hits)
-df %>%
-  filter(hits > 0, date > as.Date("2014-01-01")) %>%
-  ggplot(aes(date, hits, colour = keyword, group = keyword)) +
-  geom_line(colour = "grey80") +
-  geom_smooth(se = FALSE, span = 0.25)
+  scale_y_log10() +
+  labs(
+    title = "Google search trends",
+    x = NULL,
+    y = "Relative hits",
+    colour = "Search term"
+  )
